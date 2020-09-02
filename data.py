d1c3dde813c66c4f1fe19e12be7b91a3886a4600
kevin_wang@sjtu.edu.cn <kevin_wang@sjtu.edu.cn>
2020-09-02 19:28:43 +0800

    完成主进程收集数据的部分

---
+++ data.py
@@ -0,0 +1,10 @@
+import time
+
+
+def get_data():
+    return {
+        "alcohol": 1,
+        "ammonia": 1.5,
+        "weight": [1, 2, 3, 4],
+        "time": time.time()
+    }
--- main.py
+++ main.py
@@ -1,8 +1,25 @@
-import smbus
+import pymongo
+from multiprocessing import Process, Event
+import data
 import time
 
-bus = smbus.SMBus(1)
+
+def fetch_data(stop):
+    while not stop.is_set():
+        raw = data.get_data()
+        client = pymongo.MongoClient("127.0.0.1", 27017)
+        db = client["media_monitor"]
+        db["sensor_raw"].insert_one(raw)
+        client.close()
+        time.sleep(10)
 
 
-def read_double(addr):
-    A = bus.read_byte_data(addr)
+def daemon(stop):
+    input("Press any key to stop...\n")
+    stop.set()
+
+
+if __name__ == "__main__":
+    e = Event()
+    Process(target=fetch_data, args=(e, )).start()
+    daemon(e)
