0b24fa61e1921fed12bc8e24f1b4689e1049e54e
kevin_wang@sjtu.edu.cn <kevin_wang@sjtu.edu.cn>
2020-09-02 20:13:39 +0800

    设置守护进程

--- main.py
+++ main.py
@@ -5,13 +5,22 @@ import time
 
 
 def fetch_data(stop):
+    last_time = 0
     while not stop.is_set():
+        if time.time() - last_time < 10:
+            continue
+        else:
+            last_time = time.time()
         raw = data.get_data()
         client = pymongo.MongoClient("127.0.0.1", 27017)
         db = client["media_monitor"]
         db["sensor_raw"].insert_one(raw)
         client.close()
-        time.sleep(10)
+
+
+def web_server():
+    import web_server
+    web_server.run()
 
 
 def daemon(stop):
@@ -22,4 +31,7 @@ def daemon(stop):
 if __name__ == "__main__":
     e = Event()
     Process(target=fetch_data, args=(e, )).start()
+    web_process = Process(target=web_server)
+    web_process.daemon = True
+    web_process.start()
     daemon(e)
