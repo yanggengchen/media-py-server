b6d972b5544cec24151d151a96c964faa40fc1f0
kevin_wang@sjtu.edu.cn <kevin_wang@sjtu.edu.cn>
2020-09-02 17:02:43 +0800

    基本结构

---
+++ web_server.py
@@ -0,0 +1,12 @@
+import flask
+
+app = flask.Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+    return "{status: \"OK\", error: 0, message: \"Welcome\"}"
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
